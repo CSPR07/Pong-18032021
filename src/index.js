a1ab6a86b627880c1e6305dad4bcb5a38298d4cf
Caspar van Vuure <2140608@talnet.nl>
2021-03-18 13:01:33 +0100

    prefix hoi text in console

---
+++ src/index.js
@@ -0,0 +1,36 @@
+console.log("hoi");
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 640,
+    scale: {
+        mode: Phaser.scale.RESIZE,
+        autoCenter: Phaser.scale.CENTER_BOTH
+    },
+    scene:{
+        preload,
+        create,
+        update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false
+        }
+    }
+};
+
+const game = new Phaser.game(config);
+
+function preload(){
+
+}
+
+function create(){
+
+}
+
+function update(){
+
+}
